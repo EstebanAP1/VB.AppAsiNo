edf788c638239b97f82b2096259052cc8a0802db
Esteban Alejandro Padilla Padilla <estebanap0926@gmail.com>
2024-02-01 11:50:46 -0500

    Add database and update functions

---
+++ BD - CRUD Usuarios.sql
@@ -0,0 +1,37 @@
+USE master;
+DROP DATABASE VB_Users;
+
+CREATE DATABASE VB_Users;
+USE VB_Users;
+
+DROP TABLE IF EXISTS UserStatus;
+CREATE TABLE UserStatus(
+	id INT NOT NULL PRIMARY KEY,
+	value VARCHAR(30) NOT NULL UNIQUE
+);
+
+INSERT INTO UserStatus(id, value) VALUES 
+(1, 'Active'),
+(2, 'Inactive'),
+(3, 'Blocked');
+
+DROP TABLE IF EXISTS UserType;
+CREATE TABLE UserType(
+	id INT NOT NULL PRIMARY KEY,
+	value VARCHAR(30) NOT NULL UNIQUE
+);
+
+INSERT INTO UserType(id, value) VALUES
+(1, 'Administrador'),
+(2, 'Profesor'),
+(3, 'Estudiante');
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users(
+	id INT NOT NULL PRIMARY KEY,
+	username VARCHAR(30) NOT NULL UNIQUE,
+	password VARCHAR(30) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	status INT NOT NULL REFERENCES UserStatus(id),
+	type INT NOT NULL REFERENCES UserType(id)
+);
--- VB.AppAsiNo/FrmUsuario.vb
+++ VB.AppAsiNo/FrmUsuario.vb
@@ -5,8 +5,11 @@ Public Class FrmUsuario
     Sub New()
         InitializeComponent()
 
-        Dim route = Application.StartupPath & "\bdusuarios.accdb"
-        ConnectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={route}"
+        Dim dataSource = "MJ04EXV5\SQLEXPRESS"
+        Dim database = "VB_Users"
+        Dim user = "esteban"
+        Dim password = "1234"
+        ConnectionString = $"Provider=MSOLEDBSQL;Data Source={dataSource};Initial Catalog={database};User Id={user};Password={password}"
     End Sub
 
     Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
@@ -14,110 +17,107 @@ Public Class FrmUsuario
     End Sub
 
     Private Sub FrmUsuario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Call LoadComboUserStatus()
-        Call LoadComboUserType()
+        LoadCombo("SELECT [id],[value] FROM UserType", CmbUserStatus)
+        LoadCombo("SELECT [id],[value] FROM UserStatus", CmbUserType)
     End Sub
 
-    Private Sub LoadComboUserStatus()
-        Dim vSql = "SELECT [id],[value] FROM UserStatus"
-        Dim connection As New OleDbConnection(ConnectionString)
-        connection.Open()
-
-        Dim command As New OleDbCommand(vSql, connection)
-
-        Dim dt As New DataTable
-        Dim da As New OleDbDataAdapter(command)
-
-        da.Fill(dt)
-        connection.Close()
-
-        CmbUserStatus.DataSource = dt
-        CmbUserStatus.DisplayMember = "value"
-        CmbUserStatus.ValueMember = "id"
-        CmbUserStatus.SelectedIndex = -1
-    End Sub
-
-    Private Sub LoadComboUserType()
-        Dim vSql = "SELECT [id],[value] FROM UserType"
-        Dim connection As New OleDbConnection(ConnectionString)
-        connection.Open()
-
-        Dim command As New OleDbCommand(vSql, connection)
-
-        Dim dt As New DataTable
-        Dim da As New OleDbDataAdapter(command)
-
-        da.Fill(dt)
-        connection.Close()
-
-        CmbUserType.DataSource = dt
-        CmbUserType.DisplayMember = "value"
-        CmbUserType.ValueMember = "id"
-        CmbUserType.SelectedIndex = -1
-    End Sub
-
-    Private Sub BtnInsert_Click(sender As Object, e As EventArgs) Handles BtnInsert.Click
-        Dim vSql = "INSERT INTO User([username],[password],[name],[status],[type])
-                    VALUES (:username,:password,:name,:status,:type)"
-        Dim connection As New OleDbConnection(ConnectionString)
-        connection.Open()
-
-        Dim command As New OleDbCommand(vSql, connection)
-        command.Parameters.Add(":username", OleDbType.VarChar).Value = TxtUsername.Text
-        command.Parameters.Add(":password", OleDbType.VarChar).Value = TxtPassword.Text
-        command.Parameters.Add(":name", OleDbType.VarChar).Value = TxtName.Text
-        command.Parameters.Add(":status", OleDbType.Integer).Value = Convert.ToInt32(CmbUserStatus.SelectedValue)
-        command.Parameters.Add(":type", OleDbType.Integer).Value = Convert.ToInt32(CmbUserType.SelectedValue)
-
+    Private Sub LoadCombo(vSql, cmb)
         Try
-            command.ExecuteNonQuery()
-            MessageBox.Show($"User {TxtUsername.Text} has been created.")
-            Call CleanUserForm()
-        Catch ex As OleDbException
-            If ex.ErrorCode = -2147467259 Then
-                MessageBox.Show($"User {TxtUsername.Text} already exists.")
-                Return
-            End If
-            MessageBox.Show($"OleDb error: {ex.Message}", ex.ErrorCode.ToString())
+            Using connection As New OleDbConnection(ConnectionString)
+                connection.Open()
+
+                Dim command As New OleDbCommand(vSql, connection)
+
+                Dim dt As New DataTable
+                Dim da As New OleDbDataAdapter(command)
+
+                da.Fill(dt)
+                connection.Close()
+
+                cmb.DataSource = dt
+                cmb.DisplayMember = "value"
+                cmb.ValueMember = "id"
+                cmb.SelectedIndex = -1
+            End Using
         Catch ex As Exception
             MessageBox.Show($"Unknown error: {ex.Message}")
-        Finally
-            connection.Close()
         End Try
     End Sub
 
+    Private Function ExecuteNonQuery(vSql As String, parameters As Dictionary(Of String, Object))
+        Try
+            Using connection As New OleDbConnection(ConnectionString)
+                connection.Open()
+
+                Dim command As New OleDbCommand(vSql, connection)
+
+                For Each parameter In parameters
+                    command.Parameters.Add(parameter.Key, OleDbType.VarChar).Value = parameter.Value
+                Next
+
+                Dim result = command.ExecuteNonQuery()
+                connection.Close()
+
+                Return result
+            End Using
+        Catch ex As OleDbException
+            If ex.ErrorCode = -2147467259 Then
+                MessageBox.Show($"User {TxtUsername.Text} already exists.")
+            End If
+            MessageBox.Show($"SQL error: {ex.Message}", ex.ErrorCode.ToString())
+        Catch ex As Exception
+            MessageBox.Show($"Unknown error: {ex.Message}")
+        End Try
+
+        Return Nothing
+    End Function
+
+    Private Sub BtnInsert_Click(sender As Object, e As EventArgs) Handles BtnInsert.Click
+        If Not PasswordValidation() Then Return
+
+        Dim vSql = "INSERT INTO User([username],[password],[name],[status],[type])
+                    VALUES (@username,@password,@name,@status,@type)"
+        Dim parameters As New Dictionary(Of String, Object) From {
+            {"@username", TxtUsername.Text},
+            {"@password", TxtPassword.Text},
+            {"@name", TxtName.Text},
+            {"@status", CmbUserStatus.SelectedValue},
+            {"@type", CmbUserType.SelectedValue}
+        }
+
+        If ExecuteNonQuery(vSql, parameters) Then MessageBox.Show($"User {TxtUsername.Text} has been created.")
+        CleanUserForm()
+    End Sub
+
     Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
-        Dim vSql = "UPDATE Usuario SET [password]=:password,[name]=:name,[status]=:status,[type]=:type WHERE [username]=:username"
-        Dim connection As New OleDbConnection(ConnectionString)
-        connection.Open()
+        Dim vSql = "UPDATE Usuario SET [password]=@password,[name]=@name,[status]=@status,[type]=@type WHERE [username]=@username"
 
-        Dim command As New OleDbCommand(vSql, connection)
-        command.Parameters.Add(":password", OleDbType.VarChar).Value = TxtPassword.Text
-        command.Parameters.Add(":name", OleDbType.VarChar).Value = TxtName.Text
-        command.Parameters.Add(":status", OleDbType.Integer).Value = Convert.ToInt32(CmbUserStatus.SelectedValue)
-        command.Parameters.Add(":type", OleDbType.Integer).Value = Convert.ToInt32(CmbUserType.SelectedValue)
-        command.Parameters.Add(":username", OleDbType.VarChar).Value = TxtUsername.Text
+        Dim parameters As New Dictionary(Of String, Object) From {
+            {"@username", TxtUsername.Text},
+            {"@password", TxtPassword.Text},
+            {"@name", TxtName.Text},
+            {"@status", CmbUserStatus.SelectedValue},
+            {"@type", CmbUserType.SelectedValue}
+        }
 
-        Dim result = command.ExecuteNonQuery()
+        Dim result = ExecuteNonQuery(vSql, parameters)
 
         If result <= 0 Then
-            MessageBox.Show("User not fount.")
+            MessageBox.Show("User not found.")
         Else
             MessageBox.Show($"User {TxtUsername.Text} has been updated.")
         End If
-
-        connection.Close()
     End Sub
 
     Private Sub BtnSearchDr_Click(sender As Object, e As EventArgs) Handles BtnSearchDr.Click
         Call CleanUserForm(TxtUsername.Text)
 
-        Dim vSql = "SELECT [username],[password],[name],[status],[type] FROM User WHERE [username]=:username"
+        Dim vSql = "SELECT [username],[password],[name],[status],[type] FROM User WHERE [username]=@username"
         Dim connection As New OleDbConnection(ConnectionString)
         connection.Open()
 
         Dim command As New OleDbCommand(vSql, connection)
-        command.Parameters.Add(":username", OleDbType.VarChar).Value = TxtUsername.Text
+        command.Parameters.Add("@username", OleDbType.VarChar).Value = TxtUsername.Text
         Dim dr As OleDbDataReader = command.ExecuteReader
 
         If dr.Read() Then
@@ -137,19 +137,19 @@ Public Class FrmUsuario
     Private Sub BtnSearchDt_Click(sender As Object, e As EventArgs) Handles BtnSearchDt.Click
         Call CleanUserForm(TxtUsername.Text)
 
-        Dim vSql = "SELECT [username],[password],[name],[status],[type] FROM User WHERE [username]=:username"
+        Dim vSql = "SELECT [username],[password],[name],[status],[type] FROM User WHERE [username]=@username"
         Dim connection As New OleDbConnection(ConnectionString)
         connection.Open()
 
         Dim command As New OleDbCommand(vSql, connection)
-        command.Parameters.Add(":username", OleDbType.VarChar).Value = TxtUsername.Text
+        command.Parameters.Add("@username", OleDbType.VarChar).Value = TxtUsername.Text
 
         Dim dt As New DataTable
         Dim da As New OleDbDataAdapter(command)
         da.Fill(dt)
         connection.Close()
 
-        If dt.Rows.Count < 0 Then
+        If dt.Rows.Count = 0 Then
             MessageBox.Show("User not found.")
             Return
         End If
@@ -163,17 +163,17 @@ Public Class FrmUsuario
     End Sub
 
     Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
-        Dim vSql = "DELETE FROM User WHERE [username]=:username"
+        Dim vSql = "DELETE FROM User WHERE [username]=@username"
         Dim connection As New OleDbConnection(ConnectionString)
-        connection.Open()
 
-        Dim command As New OleDbCommand(vSql, connection)
-        command.Parameters.Add(":username", OleDbType.VarChar).Value = TxtUsername.Text
+        Dim parameters As New Dictionary(Of String, Object) From {
+            {"@username", TxtUsername.Text}
+        }
 
-        Dim result = command.ExecuteNonQuery()
+        Dim result = ExecuteNonQuery(vSql, parameters)
 
         If result <= 0 Then
-            MessageBox.Show("User not fount.")
+            MessageBox.Show("User not found.")
         Else
             MessageBox.Show($"User {TxtUsername.Text} has been deleted.")
             Call CleanUserForm()
@@ -191,7 +191,36 @@ Public Class FrmUsuario
         CmbUserType.SelectedIndex = -1
     End Sub
 
-    Private Sub PasswordValidation(sender As Object, e As EventArgs) Handles TxtPassword.KeyUp, TxtPasswordConfirm.KeyUp
+    Private Function PasswordValidation() As Boolean
+        If TxtPassword.Text <> TxtPasswordConfirm.Text Then
+            MessageBox.Show("Las contraseñas no coinciden.")
+            Return False
+        End If
+
+        If TxtPassword.Text.Length < 8 Then
+            MessageBox.Show("La contraseña debe tener al menos 8 caracteres.")
+            Return False
+        End If
+
+        If Not TxtPassword.Text.Any(Function(c) Char.IsUpper(c)) Then
+            MessageBox.Show("La contraseña debe contener al menos una letra mayúscula.")
+            Return False
+        End If
+
+        If Not TxtPassword.Text.Any(Function(c) Char.IsLower(c)) Then
+            MessageBox.Show("La contraseña debe contener al menos una letra minúscula.")
+            Return False
+        End If
+
+        If Not TxtPassword.Text.Any(Function(c) Char.IsDigit(c)) Then
+            MessageBox.Show("La contraseña debe contener al menos un dígito.")
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub PasswordKeyUpValidation(sender As Object, e As EventArgs) Handles TxtPassword.KeyUp, TxtPasswordConfirm.KeyUp
         If TxtPassword.Text <> TxtPasswordConfirm.Text Then
             BtnInsert.Enabled = False
         Else
